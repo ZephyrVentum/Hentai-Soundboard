bda2e0893e46910dd34c392d117473790204afb1
Alexander Klimenko <>
2018-12-16 16:11:58 +0200

    initial commit

---
+++ app/src/main/java/ventum/zephyr/hentaisoundboard/HentaiActivity.kt
@@ -0,0 +1,12 @@
+package ventum.zephyr.hentaisoundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HentaiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hentai)
+    }
+}
